47b8890340d836d3ea0ae0044fe68685c4cb6d11
chenxiaobin <chenxiaobin3000@gmail.com>
2016-01-25 14:31:26 +0800

    add data mgr

---
+++ engine/MyGUI/MyDataManager.h
@@ -0,0 +1,55 @@
+/**
+ * desc: 
+ * auth: chenxiaobin
+ * data: 2016-01-25
+ */
+
+#ifndef _MY_DATA_MANAGER_H_
+#define _MY_DATA_MANAGER_H_
+
+#include "MyGUI_Prerequest.h"
+#include "MyGUI_DataManager.h"
+
+namespace MyGUI {
+
+	class MyDataManager :
+		public DataManager
+	{
+	public:
+		MyDataManager();
+
+		void initialise();
+		void shutdown();
+
+		static MyDataManager& getInstance();
+		static MyDataManager* getInstancePtr();
+
+		/** @see DataManager::getData(const std::string& _name) */
+		virtual IDataStream* getData(const std::string& _name);
+
+		/** @see DataManager::isDataExist(const std::string& _name) */
+		virtual bool isDataExist(const std::string& _name);
+
+		/** @see DataManager::getDataListNames(const std::string& _pattern) */
+		virtual const VectorString& getDataListNames(const std::string& _pattern);
+
+		/** @see DataManager::getDataPath(const std::string& _name) */
+		virtual const std::string& getDataPath(const std::string& _name);
+
+	/*internal:*/
+		void addResourceLocation(const std::string& _name, bool _recursive);
+
+	private:
+		struct ArhivInfo {
+			std::wstring name;
+			bool recursive;
+		};
+		typedef std::vector<ArhivInfo> VectorArhivInfo;
+		VectorArhivInfo mPaths;
+
+		bool mIsInitialise;
+	};
+
+} // namespace MyGUI
+
+#endif // _MY_DATA_MANAGER_H_
